5dab2ddd07fdd560c0e256138de195179d22652b
cecilia <csierrab174@gmail.com>
2024-10-14 10:10:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// create string
+const message = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`//string
+console.log(message)
+
+// create 3 variables for combination
+const  firstNumber = 5 + 5;
+const secondNumber = 10 * 4;
+const thirdNumber = 40 - 1;
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(thirdNumber);
